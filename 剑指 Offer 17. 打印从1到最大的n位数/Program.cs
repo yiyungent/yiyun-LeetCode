181a8c2c8b51b1d99504b9e5fb121e52daf06420
yiyun <yiyungent@126.com>
2020-09-16 21:42:40 +0800

    feat: 剑指 Offer 24. 反转链表

---
+++ 剑指 Offer 17. 打印从1到最大的n位数/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace 剑指_Offer_17._打印从1到最大的n位数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] result = PrintNumbers(1);
+            int[] result = PrintNumbers(3);
+
+
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        #region 方法1
+        /// <summary>
+        /// 时间复杂度: O(10^n), 空间复杂度: O(1), 列表作为返回结果，不计入额外空间
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        //static int[] PrintNumbers(int n)
+        //{
+        //    int end = 1;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        end *= 10;
+        //    }
+        //    end -= 1;
+        //    int[] result = new int[end];
+        //    for (int i = 1; i <= end; i++)
+        //    {
+        //        result[i - 1] = i;
+        //    }
+
+        //    return result;
+        //} 
+        #endregion
+
+        #region 方法2: 大数打印解法
+        static int[] res;
+        static int nine = 0, count = 0, start, n;
+        static char[] num, loop = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        static int[] PrintNumbers(int n)
+        {
+            n = n;
+            res = new int[(int)Math.Pow(10, n) - 1];
+            num = new char[n];
+            start = n - 1;
+            Dfs(0);
+            return res;
+        }
+
+        static void Dfs(int x)
+        {
+            if (x == n)
+            {
+                string s = num.ToString().Substring(start);
+                if (s != "0")
+                {
+                    res[count++] = int.Parse(s);
+                }
+                if (n - start == nine)
+                {
+                    start--;
+                }
+                return;
+            }
+            foreach (char ch in loop)
+            {
+                if (ch == '9')
+                {
+                    nine++;
+                }
+                num[x] = ch;
+                Dfs(x + 1);
+            }
+            nine--;
+        }
+        #endregion
+    }
+}
---
+++ 剑指 Offer 24. 反转链表/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace 剑指_Offer_24._反转链表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 输入: 1->2->3->4->5->NULL
+            // 输出: 5->4->3->2->1->NULL
+            ListNode listNode = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            ListNode result = ReverseList(listNode);
+            while (result != null)
+            {
+                Console.Write(result.val + "->");
+                result = result.next;
+            }
+
+            Console.ReadKey();
+        }
+
+        #region 方法1: 双指针
+        //static ListNode ReverseList(ListNode head)
+        //{
+        //    // pre在cur前，cur跟随pre向前一步
+        //    ListNode cur = null, pre = head;
+        //    while (pre != null)
+        //    {
+        //        // 记下 pre 的下一步
+        //        ListNode temp = pre.next;
+        //        // pre反转指向
+        //        pre.next = cur;
+        //        // cur 向前一步
+        //        cur = pre;
+        //        // pre 向前一步
+        //        pre = temp;
+        //    }
+
+        //    return cur;
+        //}
+        #endregion
+
+        #region 方法二: 递归
+        static ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode ret = ReverseList(head.next);
+            // 在递归回退时做操作
+            // TODO: 没懂
+            head.next.next = head;
+            head.next = null;
+            return ret;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Definition for singly-linked list.
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+}
