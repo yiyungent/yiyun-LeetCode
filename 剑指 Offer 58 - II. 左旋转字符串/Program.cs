69b485f2062f7c8915ad7117ae77d25562659e43
yiyun <yiyungent@126.com>
2020-09-29 07:11:38 +0800

    feat: 剑指 Offer 58 - II. 左旋转字符串

---
+++ 剑指 Offer 58 - II. 左旋转字符串/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace 剑指_Offer_58___II._左旋转字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcdefg";
+            int k = 2;
+            Console.WriteLine(ReverseLeftWords(s, k));
+
+        }
+
+        #region 方法1: 遍历拼接
+        /// <summary>
+        /// 时间复杂度: O(n): 两次遍历共n
+        /// 空间复杂度: O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static string ReverseLeftWords(string s, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            #region 遍历方法1
+            //for (int i = n; i < s.Length; i++)
+            //{
+            //    sb.Append(s[i]);
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    sb.Append(s[i]);
+            //}
+            #endregion
+
+            #region 遍历方法2: 利用取余简化代码
+            for (int i = n; i < n + s.Length; i++)
+            {
+                sb.Append(s[i % s.Length]);
+            }
+            #endregion
+
+            return sb.ToString();
+        }
+        #endregion
+
+
+
+    }
+}
