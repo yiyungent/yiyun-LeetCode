fc586d23e945ca06ce80211f15e8e04748d5ce80
yiyun <yiyungent@126.com>
2020-10-18 13:13:39 +0800

    feat: 1480. 一维数组的动态和

---
+++ 1480. 一维数组的动态和/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1480._一维数组的动态和
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://leetcode-cn.com/problems/running-sum-of-1d-array/
+            int[] nums = { 1, 2, 3, 4 };
+            Console.WriteLine(string.Join(", ", RunningSum(nums)));
+
+        }
+
+        #region 方法1
+        /// <summary>
+        /// 时间复杂度: O(n)
+        /// 空间复杂度: O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        static int[] RunningSum(int[] nums)
+        {
+            if (nums.Length <= 1)
+            {
+                return nums;
+            }
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+        #endregion
+
+
+    }
+}
