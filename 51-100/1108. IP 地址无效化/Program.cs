51466d0096adde720a4167af58adbb513d07024a
yiyun <yiyungent@126.com>
2020-10-24 11:46:04 +0800

    feat: 1108. IP 地址无效化

---
+++ 51-100/1108. IP 地址无效化/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1108._IP_地址无效化
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://leetcode-cn.com/problems/defanging-an-ip-address/
+            string address = "1.1.1.1";
+
+            Console.WriteLine(DefangIPaddr(address));
+        }
+
+        #region 方法1
+        static string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+        #endregion
+
+    }
+}
