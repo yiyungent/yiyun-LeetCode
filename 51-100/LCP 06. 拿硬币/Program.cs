f986410ad78ea24223df8d0a0aef70a612633930
yiyun <yiyungent@126.com>
2020-10-25 13:50:02 +0800

    feat: lCP 06. 拿硬币

---
+++ 51-100/LCP 06. 拿硬币/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LCP_06._拿硬币
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 2, 1 };
+            Console.WriteLine(MinCount(nums));
+        }
+
+        #region 方法1
+        static int MinCount(int[] coins)
+        {
+            int rtn = 0;
+            foreach (var item in coins)
+            {
+                rtn += (item + 1) / 2;
+            }
+
+            return rtn;
+        }
+        #endregion
+
+    }
+}
