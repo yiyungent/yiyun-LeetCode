2e36213218c91005ca446e49a9df194b41176d79
yiyun <yiyungent@126.com>
2020-11-07 23:20:30 +0800

    feat: 1295. 统计位数为偶数的数字

---
+++ 51-100/1295. 统计位数为偶数的数字/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1295._统计位数为偶数的数字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNumbers(new int[] { 12, 345, 2, 6, 7896 }));
+        }
+
+        #region 方法1: 枚举+字符串
+        static int FindNumbers(int[] nums)
+        {
+            int rtn = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((nums[i].ToString().Length & 1) != 1)
+                {
+                    // 位数 为 偶数
+                    rtn++;
+                }
+            }
+
+            return rtn;
+        }
+        #endregion
+
+    }
+}
