d77a7283a616fff5a45d16c7d46c8ddcf5f7fc10
yiyun <yiyungent@gmail.com>
2023-10-20 19:32:05 +0800

    feat(101-150/844. 比较含退格的字符串): add

---
+++ 101-150/844. 比较含退格的字符串/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _844._比较含退格的字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://leetcode-cn.com/problems/backspace-string-compare/
+
+
+
+
+        }
+
+        #region 方法1: 双指针
+        /// <summary>
+        /// https://leetcode-cn.com/problems/backspace-string-compare/solution/bi-jiao-han-tui-ge-de-zi-fu-chuan-by-leetcode-solu/
+        /// 
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="S"></param>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public bool BackspaceCompare(string S, string T)
+        {
+
+        } 
+        #endregion
+
+
+    }
+}
